b6b4bf889fc5ef92d853a3f31e8dd2af8af9f0cf
yinzhefuyun <824299353@qq.com>
2026-07-19 02:44:08 +0800

    update PLATFORM_SH

--- platform_old.sh
+++ platform_old.sh
@@ -215,6 +215,8 @@ platform_do_upgrade_mikrotik_nand() {
 platform_do_upgrade() {
 	case "$(board_name)" in
 	8dev,jalapeno |\
+	alibaba,ap4220-48m|\
+	alibaba,ap4220-128m|\
 	aruba,ap-303 |\
 	aruba,ap-303h |\
 	aruba,ap-365 |\
@@ -232,7 +234,8 @@ platform_do_upgrade() {
 	netgear,wac510 |\
 	p2w,r619ac-64m |\
 	p2w,r619ac-128m |\
-	qxwlan,e2600ac-c2)
+	qxwlan,e2600ac-c2 |\
+	thinkplus,fogpod800)
 		nand_do_upgrade "$1"
 		;;
 	glinet,gl-b2200)
